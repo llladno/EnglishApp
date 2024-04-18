0456be64c36606373d8ae92f29a1e66a7eb1d8ab
llladno <man30968@gmail.com>
2024-04-18 12:57:07 +0300

    create structure

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import EAHeader from "@/components/layout/EAHeader/EAHeader.vue";
+</script>
+
+<template>
+<EAHeader />
+  <RouterView />
+</template>
+
+<style>
+</style>
---
+++ src/components/layout/EAHeader/EAHeader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import {RouterLink} from "vue-router";
+</script>
+
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterLink to="/registration">Login</RouterLink>
+</template>
+
+<style scoped>
+
+</style>
