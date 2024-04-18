a0a6a568ed63df3fb68ce929b977c7aa3f56bd1f
llladno <man30968@gmail.com>
2024-04-18 13:02:20 +0300

    test commit

--- src/components/layout/EAHeader/EAHeader.vue
+++ src/components/layout/EAHeader/EAHeader.vue
@@ -7,6 +7,7 @@ import {RouterLink} from "vue-router";
   <RouterLink to="/">Home</RouterLink>
   <RouterLink to="/about">About</RouterLink>
   <RouterLink to="/registration">Login</RouterLink>
+  <h1>Logo Test</h1>
 </template>
 
 <style scoped>
